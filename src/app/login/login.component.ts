13ebe5b82dfe929ec18d9e8813f3a6b027d7cda4
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-10-21 13:30:43 +0200

    Implementing Google login, logout and Displaying current user in nav-bar.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
 
-  ngOnInit() {
+  login() {
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());          // It's going to redirecting the user on one of the Auth providers like "Google, Facebook..."
   }
 
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  user: firebase.User;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+  }
 
-  ngOnInit() {
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
