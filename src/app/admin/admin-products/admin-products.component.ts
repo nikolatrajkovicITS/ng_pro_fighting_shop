b26ab7323964ad403152e8667b559a9f90597efe
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-10-26 15:45:34 +0200

    Added Searching for products functionality.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,16 +1,31 @@
+import { Product } from '../../models/product';
+import { Subscription } from 'rxjs/Subscription';
 import { ProductService } from '../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: Product[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
+  }
+
+   filter(query: string) {
+     this.filteredProducts = (query) ?
+       this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : 
+       this.products;
+   }
+
+   ngOnDestroy() {
+     this.subscription.unsubscribe();
    }
 
   ngOnInit() {
--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -15,10 +15,10 @@ export class ProductFormComponent implements OnInit {
   id;
 
   constructor(
+    private router: Router,
     private route: ActivatedRoute,
     private categoryService: CategoryService, 
-    private productService: ProductService,
-    private router: Router) {
+    private productService: ProductService) {
     this.categories$ = categoryService.getCategories();
 
     this.id = this.route.snapshot.paramMap.get('id');
