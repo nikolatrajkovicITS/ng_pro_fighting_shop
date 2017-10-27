d66de2eef3e7a412f2eeb345c9ebbd12046df624
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-10-27 12:58:43 +0200

    Implemented Data table component

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,7 +1,8 @@
-import { Product } from '../../models/product';
+import { Product } from './../../models/product';
 import { Subscription } from 'rxjs/Subscription';
-import { ProductService } from '../../product.service';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ProductService } from './../../product.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DataTableResource } from 'angular-4-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -10,23 +11,45 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[];
-  filteredProducts: any[];
   subscription: Subscription;
+  tableResource: DataTableResource<Product>;
+  items: Product[] = [];
+  itemCount: number; 
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService) { 
     this.subscription = this.productService.getAll()
-      .subscribe(products => this.filteredProducts = this.products = products);
+      .subscribe(products => {
+        this.products = products;
+        this.initializeTable(products);
+      });
   }
 
-   filter(query: string) {
-     this.filteredProducts = (query) ?
-       this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : 
-       this.products;
-   }
+  private initializeTable(products: Product[]) {
+    this.tableResource = new DataTableResource(products);
+    this.tableResource.query({ offset: 0 })
+      .then(items => this.items = items);
+    this.tableResource.count()
+      .then(count => this.itemCount = count);
+  }
 
-   ngOnDestroy() {
-     this.subscription.unsubscribe();
-   }
+  reloadItems(params) {
+    if (!this.tableResource) return;
+
+    this.tableResource.query(params)
+      .then(items => this.items = items);    
+  }
+
+  filter(query: string) { 
+    let filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+
+    this.initializeTable(filteredProducts);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
   ngOnInit() {
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,8 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import { CustomFormsModule } from 'ng2-validation'
+import { CustomFormsModule } from 'ng2-validation';
+import { DataTableModule } from 'angular-4-data-table';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -47,6 +48,7 @@ import { ProductFormComponent } from './admin/product-form/product-form.componen
     BrowserModule,
     FormsModule,
     CustomFormsModule,
+    DataTableModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     NgbModule.forRoot(),
