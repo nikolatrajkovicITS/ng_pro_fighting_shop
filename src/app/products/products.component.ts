5e9d0f2ee49623010d2c446bbcbfb117a1ab08cd
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-10-29 14:23:55 +0100

    Implemented "Filter products by Category"

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,9 @@
 import { CategoryService } from '../category.service';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-products',
@@ -8,14 +11,31 @@ import { ProductService } from '../product.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$;
+  products: Product[] = [];                    // It's good practice to initialize array 
+  filteredProducts: Product[] = [];
   categories$;
+  category: string;
 
-  constructor(private productService: ProductService, private categoryService: CategoryService) {
-    this.products$ = productService.getAll();
+  constructor(
+    private route: ActivatedRoute,                                       // We can read route parameters
+    private productService: ProductService, 
+    private categoryService: CategoryService) {
+
+    productService
+     .getAll()
+     .switchMap(products => {
+      this.products = products;
+      return  route.queryParamMap;
+     })
+      .subscribe(params => {
+        this.category = params.get('category');                             // Read category from our route paramater
+     
+        this.filteredProducts = (this.category) ?                           // If there is category
+          this.products.filter(p => p.category === this.category) :         // we are going to call filter method on our products array 
+          this.products;                                                    // : (otherwise) if you don't have category, we want return all products.
+        }); 
+
     this.categories$ = categoryService.getAll();
   }
-
-
 
 }
