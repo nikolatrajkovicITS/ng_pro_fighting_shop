8652f75a62569ec9257d88d15ef9dc0dc87c4745
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-11-01 20:18:52 +0100

    Implemented AddToCart functionality and refactoring the code.

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -14,16 +14,6 @@ export class ProductCardComponent {
   constructor(private shoppingCartService: ShoppingCartService) { }
 
   addToCart(product: Product) {
-    let cartId = localStorage.getItem('cartId');
-    if (!cartId) {
-      this.shoppingCartService.create().then(result => {    // This create method retruns a prommise, we can call "then" get result => 
-        localStorage.setItem('cartId', result.key);         // at this point we need to store this new ID at the localStorage
-              
-        // Add product to cart
-      });
-    } else {
-      // Add product to cart
-    }
+    this.shoppingCartService.addToCart(product); 
   }
-
 }
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,15 +1,39 @@
+import { Product } from './models/product';
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import 'rxjs/add/operator/take';
 
 @Injectable()
 export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  create() {
+  private create() {
     return this.db.list('/shopping-carts').push({
       dateCreated: new Date().getTime()
     })
   }
 
+  private getCart(cartId: string) {
+    return this.db.object('/shopping-carts/' + cartId);
+  }
+
+  private async getOrCreateCardId() {
+    let cartId = localStorage.getItem('cartId');
+    if (cartId) return cartId;
+    
+    let result = await this.create();
+    localStorage.setItem('cartId', result.key);         // at this point we need to store this new ID at the localStorage       
+    return result.key;
+  }
+
+  async addToCart(product: Product) {
+    let cartId = await this.getOrCreateCardId();
+    let item$ = this.db.object('/shopping-carts/' + cartId + '/items/' + product.$key);
+    item$.take(1).subscribe(item => {
+      if (item.$exists()) item$.update({ quantity: item.quantity + 1 });
+      else item$.set({ product: product, quantity: 1 });
+    });
+
+  }
 }
