04d983030e969b5fb58c31139398af8452be16b1
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-11-05 21:54:38 +0100

    Displaying the total price of added items in ShoppingCart and Refactoring ProductQuantityComponent.

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,11 +1,26 @@
 import { ShoppingCartItem } from './shopping-cart-item';
+import { Product } from './product';
 
 export class ShoppingCart {
   items: ShoppingCartItem[] = [];
 
   constructor(public itemsMap: { [productId: string]: ShoppingCartItem}) {
-    for (let productId in itemsMap)
-      this.items.push(itemsMap[productId]);
+    for (let productId in itemsMap) {
+      let item = itemsMap[productId];
+      this.items.push(new ShoppingCartItem(item.product, item.quantity));
+    }
+  }
+
+  getQuantity(product: Product) {                  // We are asking what is the quantity of this product
+    let item = this.itemsMap[product.$key];
+    return item ? item.quantity : 0;               // If we have Item return "item.quantity", otherwise return 0;
+  }
+
+  get totalPrice() {
+    let sum = 0;
+    for (let productId in this.items)
+      sum += this.items[productId].totalPrice;
+    return sum;
   }
   
   get totalItemsCount() {
